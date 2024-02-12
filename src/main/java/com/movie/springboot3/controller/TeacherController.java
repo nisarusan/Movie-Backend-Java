832dc456ffcdb748c4626047c5a63c873f9f0f4b
Nisaru Abak <nizar.abak@live.nl>
2024-02-12 07:15:30 +0100

    Add Repository methods

--- src/main/java/com/movie/springboot3/controller/TeacherController.java
+++ src/main/java/com/movie/springboot3/controller/TeacherController.java
@@ -4,9 +4,13 @@ import com.movie.springboot3.model.Teacher;
 import com.movie.springboot3.repository.TeacherRepository;
 import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
+import java.time.LocalDate;
 import java.util.List;
 
 @RestController
@@ -21,9 +25,17 @@ public class TeacherController {
          return ResponseEntity.ok(teacherRepository.findAll());
     }
 
+    @GetMapping("/after")
+    public ResponseEntity<List<Teacher>> getTeachersAfter(@RequestParam LocalDate date) {
+        return ResponseEntity.ok(teacherRepository.findByDobAfter(date));
+    }
+
     @PostMapping
     public ResponseEntity<Teacher> addTeacher(@RequestBody Teacher teacher) {
-       return ResponseEntity.ok(teacherRepository.save(teacher));
+        teacherRepository.save(teacher);
+        URI uri = URI.create(
+                ServletUriComponentsBuilder.fromCurrentRequestUri().path("/" + teacher.getId()).toUriString());
+       return ResponseEntity.created(uri).body(teacher);
     }
 
 }
--- src/main/java/com/movie/springboot3/repository/TeacherRepository.java
+++ src/main/java/com/movie/springboot3/repository/TeacherRepository.java
@@ -3,6 +3,9 @@ package com.movie.springboot3.repository;
 import com.movie.springboot3.model.Teacher;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface TeacherRepository extends JpaRepository<Teacher, Long> {
+import java.time.LocalDate;
+import java.util.List;
 
+public interface TeacherRepository extends JpaRepository<Teacher, Long> {
+    public List<Teacher> findByDobAfter(LocalDate date);
 }
